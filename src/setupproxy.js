0c4a4a1d82d9e867b534535d30aa054f29ae4214
Hyunkim <zhyunz123@gmial.com>
2023-10-14 18:23:30 +0900

    first

---
+++ src/setupproxy.js
@@ -0,0 +1,12 @@
+// const proxy = require("http-proxy-middleware");
+
+// // src/setupProxy.js
+// const { createProxyMiddleware } = require("http-proxy-middleware");
+// module.exports = function (app) {
+//   app.use(
+//     createProxyMiddleware("/v1", {
+//       target: "http://192.168.0.179:8099",
+//       changeOrigin: true,
+//     })
+//   );
+// };
